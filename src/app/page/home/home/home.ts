019717fb3cde7826893c148af649574972da4251
Douglas Prado <douglas.prado@forlogic.net>
2025-09-09 18:21:19 -0300

    create  app-under-construction

--- src/app/page/home/home/home.ts
+++ src/app/page/home/home/home.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Header } from "../../shared/header/header";
 import { Footer } from "../../shared/footer/footer";
 import { Page } from "../../shared/page/page";
+import { UnderConstruction } from "../../shared/under-construction/under-construction";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, Header, Footer, Page],
+  imports: [CommonModule, MatCardModule, MatButtonModule, Header, Footer, Page, UnderConstruction],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
 export class Home {
+  IsDev: boolean = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      const value = localStorage.getItem('IsDev');
+      this.IsDev = value === 'true';
+    }
+  }
+
+  toggleProduction() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.IsDev = !this.IsDev;
+      localStorage.setItem('IsDev', this.IsDev.toString());
+    }
+  }
 }
+
---
+++ src/app/page/shared/under-construction/under-construction.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-construction',
+  imports: [],
+  templateUrl: './under-construction.html',
+  styleUrl: './under-construction.scss'
+})
+export class UnderConstruction {
+ reloadPage() {
+    window.location.reload();
+  }
+}
